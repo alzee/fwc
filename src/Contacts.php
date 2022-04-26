09a25fa5b98931627011de2c05c9796a9401306d
Al Zee <z@alz.ee>
2022-04-26 10:03:47 +0800

    nth

---
+++ src/Contacts.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * vim:ft=php et ts=4 sts=4
+ * @author Al Zee <z@alz.ee>
+ * @version
+ * @todo
+ */
+
+namespace Alzee\Fwc;
+
+// use What;
+
+class Contacts
+{
+    private $a;
+    private $b;
+    private $c;
+    
+    public function __construct()
+    {
+    }
+
+    public function __destruct()
+    {
+    }
+}
+
