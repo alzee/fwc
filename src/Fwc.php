c2acb6214595a56e44e187c887963d8936767462
Al Zee <z@alz.ee>
2022-04-26 10:10:17 +0800

    nth

---
+++ src/Fwc.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * vim:ft=php et ts=4 sts=4
+ * @author Al Zee <z@alz.ee>
+ * @version
+ * @todo
+ */
+
+namespace Alzee\Fwc;
+
+// use What;
+
+class Fwc
+{
+    private $a;
+    private $b;
+    private $c;
+    
+    public function __construct()
+    {
+    }
+
+    public function getAccessToken()
+    {
+    }
+
+    public function __destruct()
+    {
+    }
+}
+
